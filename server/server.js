14eaf95f7db8cc57b1490d002bb75e6e6eab06bb
Lan <lanlehoang10@gmail.com>
2019-02-08 17:21:37 +0700

    chat app

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const path = require('path');
 const publicPath = path.join(__dirname,'../public');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO.listen(server);
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+    socket.on('createMessage',(message)=>{
+        console.log('createMessage',message);
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+    })
+    socket.on('disconnect',()=>{
+        console.log('user was disconnected');
+    })
+})
+server.listen(port,()=>{
     console.log(`started on port ${port}`);
 });
